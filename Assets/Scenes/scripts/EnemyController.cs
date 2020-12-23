fe3439b0b37ce5fcb35fc62c9d4fde6a1b23a8e9
Torogeldiev-T <tilek2402@gmail.com>
2020-12-24 00:06:15 +0300

    project2-initial

---
+++ Assets/Scenes/scripts/EnemyController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public enum EnemyState
+{
+    Patrolling,
+    Chasing,
+    Returning,
+    Attacking
+}
+
+public class EnemyController : MonoBehaviour
+{
+    public float MinDistanceToFollow = 10.0f;
+    public float MinDistanceToPatrolPoint = 2.0f;
+    public float MinDistanceToAttack = 0.5f;
+
+    public GameObject PointToPatroll;
+    public float patrollAreaRadius = 4.0f;
+    public GameObject target;
+
+    NavMeshAgent agent;
+    EnemyState state;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        state = EnemyState.Patrolling;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+                break;
+            case EnemyState.Chasing:
+                Chase();
+                break;
+            case EnemyState.Attacking:
+                Attack();
+                break;
+            case EnemyState.Returning:
+                Return();
+                break;
+        }
+    }
+
+    private void Patrol()
+    {
+        if (!agent.hasPath)
+        {
+            Vector2 randomPoint = Random.insideUnitCircle;
+            randomPoint *= patrollAreaRadius;
+
+            if (PointToPatroll != null)
+            {
+                Vector3 destination = PointToPatroll.transform.position;
+                destination.x += randomPoint.x;
+                destination.z += randomPoint.y;
+
+                agent.SetDestination(destination);
+            }
+        }
+
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < MinDistanceToFollow)
+        {
+            state = EnemyState.Chasing;
+            agent.ResetPath();
+        }
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(target.transform.position);
+
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance >= MinDistanceToFollow)
+        {
+            state = EnemyState.Returning;
+            agent.ResetPath();
+        }
+        else if (distance <= MinDistanceToAttack)
+        {
+            state = EnemyState.Attacking;
+            agent.ResetPath();
+        }
+    }
+
+    private void Attack()
+    {
+        // TODO
+
+        state = EnemyState.Returning;
+        agent.ResetPath();
+    }
+
+    private void Return()
+    {
+        if (!agent.hasPath)
+        {
+            if (PointToPatroll != null)
+            {
+                Vector3 destination = PointToPatroll.transform.position;
+                agent.SetDestination(destination);
+            }
+        }
+
+        if (Vector3.Distance(transform.position, target.transform.position) < MinDistanceToFollow)
+        {
+            state = EnemyState.Chasing;
+            agent.ResetPath();
+        }
+
+
+        if (PointToPatroll != null)
+        {
+            if (Vector3.Distance(transform.position, PointToPatroll.transform.position) < MinDistanceToPatrolPoint)
+            {
+                state = EnemyState.Patrolling;
+                agent.ResetPath();
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/scripts/collectableController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class collectableController : MonoBehaviour
+{
+    public Text score;
+    int currentScore = 0;
+    public GameObject obj;
+    public GameObject prefap;
+    void Start()
+    {
+         score.text = currentScore.ToString();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        GameObject gameObject = collision.gameObject;
+        if(gameObject.CompareTag("Player")){
+            Destroy(obj);
+            currentScore = int.Parse(score.text);
+            currentScore++;
+            float x = Random.Range(-3,3f);
+            float y = Random.Range(5f,5.5f);
+            float z = Random.Range(-3f,3f);
+            Instantiate(prefap,new Vector3(x,y,z),Quaternion.Euler(0.0f, 5f, 133f));
+            score.text = currentScore.ToString();
+        }
+                
+    }
+
+    void Update()
+    {
+        
+    }
+}
